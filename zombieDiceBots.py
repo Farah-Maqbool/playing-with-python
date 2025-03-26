9225c7c96ceaf9faeecf573914a6d55832e58087
Farah Maqbool <farahprogrammer23@gmail.com>
2025-03-26 12:08:17 +0500

    Update zombieDiceBots.py

--- zombieDiceBots.py
+++ zombieDiceBots.py
@@ -22,15 +22,24 @@ class RandomBot():
         self.name = name
         
     def turn(self, gameState):
+        diceRollResults = zombiedice.roll()
+
         
-        ...
+        while diceRollResults is not None:
+            decide = random.randint(0,1)
+            if decide == 1:
+                diceRollResults = zombiedice.roll()
+            else:
+                break
+    
 
 zombies = (
-    zombiedice.examples.RandomCoinFlipZombie(name='Random'),
+    # zombiedice.examples.RandomCoinFlipZombie(name='Random'),
     zombiedice.examples.RollsUntilInTheLeadZombie(name='Until Leading'),
     zombiedice.examples.MinNumShotgunsThenStopsZombie(name='Stop at 2 Shotguns', minShotguns=2),
     zombiedice.examples.MinNumShotgunsThenStopsZombie(name='Stop at 1 Shotgun', minShotguns=1),
     MyZombie(name='My Zombie Bot'),
+    RandomBot(name="My Random Zombie")
     # Add any other zombie players here.
 )
 
