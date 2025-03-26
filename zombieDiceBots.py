d4c0fe19874b59af9620e57bc49fb9e1c5549984
Farah Maqbool <farahprogrammer23@gmail.com>
2025-03-26 12:29:10 +0500

    Update zombieDiceBots.py

--- zombieDiceBots.py
+++ zombieDiceBots.py
@@ -33,13 +33,72 @@ class RandomBot():
                 break
     
 
+class StopTwoB():
+
+    def __init__(self, name):
+        self.name = name
+
+    def turn(self, gameState):
+        diceRollResults = zombiedice.roll()
+
+        brain = 0
+
+        while diceRollResults is not None:
+            brain += diceRollResults['brains']
+
+            if brain < 2:
+                diceRollResults = zombiedice.roll()
+            else:
+                break
+
+
+class StopTwoSG():
+
+    def __init__(self, name):
+        self.name = name
+
+    def turn(self, gameState):
+        diceRollResults = zombiedice.roll()
+
+        shotgun = 0
+
+        while diceRollResults is not None:
+            shotgun += diceRollResults['shotgun']
+
+            if shotgun < 2:
+                diceRollResults = zombiedice.roll()
+            else:
+                break
+
+class SGMorethanBrain():
+    def __init__(self, name):
+        self.name = name
+
+    def turn(self, gameState):
+        diceRollResults = zombiedice.roll()
+
+        brain = 0
+        shotgun = 0
+
+        while diceRollResults is not None:
+            brain += diceRollResults['brains']
+            shotgun += diceRollResults['shotgun']
+
+            if brain < shotgun:
+                diceRollResults = zombiedice.roll()
+            else:
+                break
+
 zombies = (
     # zombiedice.examples.RandomCoinFlipZombie(name='Random'),
-    zombiedice.examples.RollsUntilInTheLeadZombie(name='Until Leading'),
-    zombiedice.examples.MinNumShotgunsThenStopsZombie(name='Stop at 2 Shotguns', minShotguns=2),
-    zombiedice.examples.MinNumShotgunsThenStopsZombie(name='Stop at 1 Shotgun', minShotguns=1),
+    # zombiedice.examples.RollsUntilInTheLeadZombie(name='Until Leading'),
+    # zombiedice.examples.MinNumShotgunsThenStopsZombie(name='Stop at 2 Shotguns', minShotguns=2),
+    # zombiedice.examples.MinNumShotgunsThenStopsZombie(name='Stop at 1 Shotgun', minShotguns=1),
     MyZombie(name='My Zombie Bot'),
-    RandomBot(name="My Random Zombie")
+    RandomBot(name="My Random Zombie"),
+    StopTwoB(name="Stop at 2 brain"),
+    StopTwoSG(name="Stop at 2 SG"),
+    SGMorethanBrain(name="More than brain")
     # Add any other zombie players here.
 )
 
